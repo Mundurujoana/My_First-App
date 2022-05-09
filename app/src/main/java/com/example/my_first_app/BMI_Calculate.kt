6d23454988b7241d217d55eb733fb68f7d7db48e
Mundurujoana <mundujoana@gmail.com>
2022-05-09 12:01:41 +0300

    first commit

---
+++ app/src/main/java/com/example/my_first_app/BMI_Calculate.kt
@@ -0,0 +1,11 @@
+package com.example.my_first_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BMI_Calculate : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bmi_calculate)
+    }
+}
